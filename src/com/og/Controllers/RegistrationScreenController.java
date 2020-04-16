2faf6a228699f74d892ef4341d9e0c2795706863
comougi <spsprwrw@gmail.com>
2020-04-16 19:18:31 +0300

    update registration method

--- src/com/og/Controllers/RegistrationScreenController.java
+++ src/com/og/Controllers/RegistrationScreenController.java
@@ -22,9 +22,9 @@ public class RegistrationScreenController extends WorkWIthXML {
 
         if (login.trim().length() == 0 || password.trim().length() == 0 || fullName.trim().length() == 0 || position.trim().length() == 0) {
             Alert alert = new Alert(Alert.AlertType.WARNING);
-            alert.setTitle("LogIn Failed");
-            alert.setHeaderText("Unable to Log In");
-            alert.setContentText("Login or Password were empty\nPlease Try Again.");
+            alert.setTitle("Registration Failed");
+            alert.setHeaderText("Unable to registration");
+            alert.setContentText("Login, Password, Position or FullName were empty\nPlease Try Again.");
             alert.showAndWait();
             loginField.clear();
             passwordField.clear();
@@ -33,8 +33,31 @@ public class RegistrationScreenController extends WorkWIthXML {
             return;
         }
 
+        switch (position) {
+            case "admin":
+                addUser(login, password, position, fullName);
+                break;
+            case "seller":
+                addUser(login, password, position, fullName);
+                break;
+            case "store keeper":
+                addUser(login, password, position, fullName);
+                break;
+            default:
+                Alert alert = new Alert(Alert.AlertType.WARNING);
+                alert.setTitle("Registration Failed");
+                alert.setHeaderText("Unable to registration");
+                alert.setContentText("Use only 'admin' , 'seller' or 'store keeper'\nPlease Try Again.");
+                alert.showAndWait();
+                loginField.clear();
+                passwordField.clear();
+                fullNameField.clear();
+                positionField.clear();
+                return;
+        }
 
 
-        addUser(login, password, position, fullName);
+
+
     }
 }
